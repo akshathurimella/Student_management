e8cb4834a703889a49de26aff77145094eeec752
akshathurimella <155985562+akshathurimella@users.noreply.github.com>
2026-06-26 18:02:56 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -4,3 +4,18 @@ JOIN enrollments e
     ON s.stu_id = e.stu_id
 JOIN courses c
     ON e.course_id = c.course_id;
+
+
+select students.stu_id,students.course_id,courses.course_id,courses.course_name
+    -> from students
+    -> right join
+    -> courses
+    -> on students.course_id = courses.course_id;
++--------+-----------+-----------+--------------+
+| stu_id | course_id | course_id | course_name  |
++--------+-----------+-----------+--------------+
+|      4 |       101 |       101 | SQL          |
+|      2 |       102 |       102 | Python       |
+|      3 |       103 |       103 | Data Science |
+|      1 |       104 |       104 | Java         |
++--------+-----------+-----------+--------------+
