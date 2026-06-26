5389651fb775fb52ab5bf776c03467ce8a0c9a3a
akshathurimella <155985562+akshathurimella@users.noreply.github.com>
2026-06-26 17:39:08 +0530

    insert_tables.sql

---
+++ insert_tables.sql
@@ -0,0 +1,28 @@
+ INSERT INTO students VALUES
+    -> (1,"aksha","aksha@gmail.com","hyderabad","2026-04-12");
+
+ INSERT INTO students VALUES
+    -> (2,"Raj","raj@gmail.com","Vizag","2025-07-22");
+
+INSERT INTO students VALUES
+    -> (3,"Anshu","Anshu@gmail.com","chennai","2026-03-19");
+
+ INSERT INTO students VALUES
+    -> (4,"Sneha","Sneha@gmail.com","vijayawada","2025-12-20");
+
+INSERT INTO students VALUES
+    -> (5,"kumar","Kumar@gmail.com","Kolkata","2024-11-03");
+
+
+
+insert into courses values(101,"SQL",5000);
+
+insert into courses values(102,"Python",4500),(103,"Data Science",6000);
+
+insert into courses values(104,"Java",7000),(105,"machine learning",5500);
+
+
+
+insert into enrollments values(11,1,101);
+insert into enrollments values(22,2,102);
+insert into enrollments values(33,3,103),(44,4,104),(55,5,105);
